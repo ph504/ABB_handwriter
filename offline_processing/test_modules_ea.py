b57c70a46d953099f4e9aa540a1f1116f0f73cd7
Arya Parvizi <aresparvizi@gmail.com>
2022-08-12 12:13:33 +0430

    added and renamed files of ppgea and ea

--- offline_processing/test_modules_ea.py
+++ offline_processing/test_modules_ea.py
@@ -7,7 +7,7 @@
 
 
 
-import ppg_ea
+import evolutionary_algorithms as ea
 import random
 
 
@@ -50,7 +50,7 @@ def test_insert_mutation():
     test_num = TEST_NUM
     for i in range(test_num):
         print('\n =================== i= ', i+1)
-        ppg_ea.insert_mutation(test_child)
+        ea.insert_mutation(test_child)
         print(test_child)
 
 
@@ -66,7 +66,7 @@ def test_scramble_mutation():
     test_num = TEST_NUM
     for i in range(test_num):
         print('\n =================== i= ', i+1)
-        ppg_ea.scramble_mutation(test_child)
+        ea.scramble_mutation(test_child)
         print(test_child)
 
 
@@ -82,7 +82,7 @@ def test_inverse_mutation():
     test_num = TEST_NUM
     for i in range(test_num):
         print('\n =================== i= ', i+1)
-        ppg_ea.inverse_mutation(test_child)
+        ea.inverse_mutation(test_child)
         print(test_child)
 
 
@@ -98,7 +98,7 @@ def test_swap_mutation():
     test_num = TEST_NUM
     for i in range(test_num):
         print('\n =================== i= ', i+1)
-        ppg_ea.swap_mutation(test_child)
+        ea.swap_mutation(test_child)
         print(test_child)
 
 
@@ -126,7 +126,7 @@ def test_generate_parents(test_parents_num=79, out_option=False):
 
 
 def test_duplocal_parentspair(test_parents, out_option=False):
-    test_result_duplocal = ppg_ea.get_parents_pair_duplocal(test_parents)
+    test_result_duplocal = ea.get_parents_pair_duplocal(test_parents)
     if(out_option):
         for x in test_result_duplocal:
             print(x)
@@ -137,7 +137,7 @@ def test_duplocal_parentspair(test_parents, out_option=False):
 
 
 def test_dupglobal_parentspair(test_parents, out_option=False):
-    test_result_dupglobal = ppg_ea.get_parents_pair_dupglobal(test_parents)
+    test_result_dupglobal = ea.get_parents_pair_dupglobal(test_parents)
     if(out_option):
         for x in test_result_dupglobal:
             print(x)
@@ -148,7 +148,7 @@ def test_dupglobal_parentspair(test_parents, out_option=False):
 
 
 def test_nodup_parentspair(test_parents, out_option=False):
-    test_result_nodup = ppg_ea.get_parents_pair_nodup(test_parents)
+    test_result_nodup = ea.get_parents_pair_nodup(test_parents)
     if(out_option):
         for x in test_result_nodup:
             print(x)
@@ -159,12 +159,12 @@ def test_nodup_parentspair(test_parents, out_option=False):
 
 
 def test_uniqpair_parentspair(test_parents, out_option=False, test_num=TEST_NUM):
-    ppg_ea.DEBUGMODE = True
+    ea.DEBUGMODE = True
     okay_counter = 0
     for i in range(test_num):
         if(out_option):
             print('test', i, ':')
-        test_result_uniqpair, test_result_uniqpair_i = ppg_ea.get_parents_pair_uniqpair(test_parents)
+        test_result_uniqpair, test_result_uniqpair_i = ea.get_parents_pair_uniqpair(test_parents)
         if(out_option):
             set_test_result_uniqpair_i = set(test_result_uniqpair_i)
             if(len(set_test_result_uniqpair_i)==len(test_result_uniqpair_i)):
@@ -174,7 +174,7 @@ def test_uniqpair_parentspair(test_parents, out_option=False, test_num=TEST_NUM)
         print('out of', test_num, 'tests,', okay_counter, 'successfully had no duplicate pairs!')
     #     for x in test_result_uniqpair:
     #         print(x)
-    ppg_ea.DEBUGMODE = False
+    ea.DEBUGMODE = False
     return test_result_uniqpair
 
 
@@ -182,7 +182,7 @@ def test_uniqpair_parentspair(test_parents, out_option=False, test_num=TEST_NUM)
 
 
 def test_primeorder_parentspair(test_parents, out_option=False):
-    test_result_prime_order = ppg_ea.get_parents_pair_prime_order(test_parents)
+    test_result_prime_order = ea.get_parents_pair_prime_order(test_parents)
     if(out_option):
         for x in test_result_prime_order:
             print(x)
@@ -202,14 +202,14 @@ def test_primeorder_parentspair(test_parents, out_option=False):
 # *random pairs, no restriction on duplicates
 # *unchanged order fo the pool paired.
 def test_o1x(parentspairs, test_num=TEST_NUM, out_option=False, rd=-1, ld=-1):
-    ppg_ea.DEBUGMODE=True
+    ea.DEBUGMODE=True
     for i in range(test_num):
-        test_o1x_children = ppg_ea.order1_crossover(parentspairs, rd=rd, ld=ld)
+        test_o1x_children = ea.order1_crossover(parentspairs, rd=rd, ld=ld)
         if(out_option):
             print('resulted children from order 1 crossover:')
             for child in test_o1x_children:
                 print(child)
-    ppg_ea.DEBUGMODE=True
+    ea.DEBUGMODE=True
     return test_o1x_children
 
 
@@ -226,14 +226,14 @@ def test_o1x(parentspairs, test_num=TEST_NUM, out_option=False, rd=-1, ld=-1):
 # *random pairs, no restriction on duplicates
 # *unchanged order fo the pool paired.
 def test_pmx(parentspairs, test_num=TEST_NUM, out_option=False, rd=-1, ld=-1):
-    ppg_ea.DEBUGMODE = True
+    ea.DEBUGMODE = True
     for i in range(test_num):
-        test_pmx_children = ppg_ea.partially_mapped_crossover(parentspairs, rd=rd, ld=ld)
+        test_pmx_children = ea.partially_mapped_crossover(parentspairs, rd=rd, ld=ld)
         if(out_option):
             print('resulted children from partially mapped crossover:')
             for child in test_pmx_children:
                 print(child)
-    ppg_ea.DEBUGMODE = False
+    ea.DEBUGMODE = False
     return test_pmx_children
 
 
@@ -250,14 +250,14 @@ def test_pmx(parentspairs, test_num=TEST_NUM, out_option=False, rd=-1, ld=-1):
 # *random pairs, no restriction on duplicates
 # *unchanged order fo the pool paired.
 def test_cx(parentspairs, test_num=TEST_NUM, out_option=False):
-    ppg_ea.DEBUGMODE = True
+    ea.DEBUGMODE = True
     for i in range(test_num):
-        test_cx_children = ppg_ea.cycle_crossover(parentspairs)
+        test_cx_children = ea.cycle_crossover(parentspairs)
         if(out_option):
             print('resulted children from cycle crossover')
             for child in test_cx_children:
                 print(child)
-    ppg_ea.DEBUGMODE = False
+    ea.DEBUGMODE = False
     return test_cx_children
 
 
@@ -274,14 +274,14 @@ def test_cx(parentspairs, test_num=TEST_NUM, out_option=False):
 # *random pairs, no restriction on duplicates
 # *unchanged order fo the pool paired.
 def test_ejx(parentspairs, test_num=TEST_NUM, out_option=False):
-    ppg_ea.DEBUGMODE = True
+    ea.DEBUGMODE = True
     for i in range(test_num):
-        test_ejx_children = ppg_ea.edge_recombination(parentspairs)
+        test_ejx_children = ea.edge_recombination(parentspairs)
         if(out_option):
             print('resulted children from edge recombination')
             for child in test_ejx_children:
                 print(child)
-    ppg_ea.DEBUGMODE = False
+    ea.DEBUGMODE = False
     return test_ejx_children
 
 
@@ -325,9 +325,9 @@ def main():
 
 if __name__ == '__main__':
     main()
-    test_parents_pool = test_generate_parents(test_parents_num=6)
-    test_parentspairs = test_nodup_parentspair(test_parents_pool)
-    children = test_pmx(test_parentspairs, test_num=1, out_option=True)
+    # test_parents_pool = test_generate_parents(test_parents_num=6)
+    # test_parentspairs = test_nodup_parentspair(test_parents_pool)
+    # children = test_pmx(test_parentspairs, test_num=1, out_option=True)
 
     
 
