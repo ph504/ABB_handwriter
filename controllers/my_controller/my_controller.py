1feadbb89c27018bb802908fb6b53ad548714df9
Arya Parvizi <aresparvizi@gmail.com>
2022-08-25 13:13:37 +0430

    final results from ea

--- controllers/my_controller/my_controller.py
+++ controllers/my_controller/my_controller.py
@@ -44,7 +44,7 @@ data_dir = '..\\..\\data\\path(t) numbers\\'
 data_fname = 'patht_Num_ea_'
 
 DRAWING_TIME = 2 * math.pi + 1.5
-DIGIT = 2
+DIGIT = 9
 #############################################
 
 # Initialize the Webots Supervisor.
